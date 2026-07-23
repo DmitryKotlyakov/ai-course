179a91bf13493359748ae47fbe695eb9035572f1
Aquat <aquaticocellot>
2026-07-23 16:27:23 +0300

    Simplify lesson routes with directory pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,3 @@ document.querySelectorAll('[data-checklist]').forEach((list) => {
     list.dataset.complete = [...inputs].every((item) => item.checked) ? 'true' : 'false';
   }));
 });
-
-// Страницы уроков используют тот же формат ссылок, что и основная навигация.
-document.querySelectorAll('a.brand[href="../index.html"]').forEach((link) => {
-  link.setAttribute('href', '/');
-});
-document.querySelectorAll('a[href="../index.html"], a[href="../index.html#program"]').forEach((link) => {
-  if (!link.classList.contains('brand')) link.setAttribute('href', '/#program');
-});
